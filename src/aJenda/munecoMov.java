f192695a4a6ba8ac330037ecd9dd431c6e0a951a
Tomás Generelo <tgenerelo@gmail.com>
2020-11-25 14:34:16 +0100

    Añadido MuñecoMov

---
+++ src/aJenda/munecoMov.java
@@ -0,0 +1,57 @@
+package aJenda;
+
+import java.time.Clock;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class munecoMov {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		for (int i=0; i<10000; i++) {
+			if (1==1000 || i==3000 || i==5000) {
+				System.out.println("  _______________________");
+				System.out.println(" |                       |");
+				System.out.println(" |       ________        |");
+				System.out.println(" |       | /             |");
+				System.out.println(" |       |/   \\O/        |");
+				System.out.println(" |       |     |         |");
+				System.out.println(" |       |   _/_\\_       |");
+				System.out.println(" |      /|\\  |   |       |");
+				System.out.println(" |     /_|_\\_|___|_      |");
+				System.out.println(" |                       |");
+				System.out.println(" |_______________________|");
+			} else {
+				if (i==2000 || i==4000 || i==6000) {
+					System.out.println("  _______________________");
+					System.out.println(" |                       |");
+					System.out.println(" |       ________        |");
+					System.out.println(" |       | /             |");
+					System.out.println(" |       |/    O         |");
+					System.out.println(" |       |    /|\\        |");
+					System.out.println(" |       |   _/_\\_       |");
+					System.out.println(" |      /|\\  |   |       |");
+					System.out.println(" |     /_|_\\_|___|_      |");
+					System.out.println(" |                       |");
+					System.out.println(" |_______________________|");
+				}
+				
+			}
+			
+
+			
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
